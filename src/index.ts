a467185809dc8511aa60d0812de289775c4bf41c
Johnny Gérard <john@jgerard.dev>
2024-04-18 08:44:03 +0200

    feat: update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // This program is a quine (self-replicating program)
 import { readFile } from 'node:fs/promises';
 
-const data = await readFile('src/index.ts', 'utf-8');
+const data = await readFile('src/index.ts', { encoding: 'utf8' });
 
 console.log(data);
